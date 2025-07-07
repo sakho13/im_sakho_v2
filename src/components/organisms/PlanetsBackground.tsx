d78054582d87d1867e6b2ea137e9ebbab14f0a52
sakho13 <wecl.khotaro@gmail.com>
2025-07-07 21:03:11 +0900

    ✨ 背景アニメーションのパラメータを調整し、動きの滑らかさを向上

--- src/components/organisms/PlanetsBackground.tsx
+++ src/components/organisms/PlanetsBackground.tsx
@@ -6,25 +6,31 @@ import { AnimBase } from "@/components/atoms/animation/AnimBase"
 
 export function PlanetsBackground() {
   const requestRef = useRef<number>(0)
-  const posRef = useRef({ x: 200, y: 0 })
-  const velRef = useRef({ x: 0, y: -1 })
+  const posRef = useRef({ x: 400, y: 0 })
+  const velRef = useRef({ x: 0, y: -1.4 })
+
+  const rotateSumRef = useRef(0)
+  const rotatePlanetRef = useRef(0)
+
   const [, setTick] = useState(0)
 
   useEffect(() => {
-    const dt = 1 / 60
-    const GM = 4000
+    const dt = 1
+    const GM = 1000
 
     const step = () => {
       const p = posRef.current
       const v = velRef.current
       const r = Math.sqrt(p.x * p.x + p.y * p.y)
-      const ax = (-GM * p.x) / (r ** 3)
-      const ay = (-GM * p.y) / (r ** 3)
+      const ax = (-GM * p.x) / r ** 3
+      const ay = (-GM * p.y) / r ** 3
 
       v.x += ax * dt
       v.y += ay * dt
       p.x += v.x * dt
       p.y += v.y * dt
+      rotateSumRef.current += -0.1
+      rotatePlanetRef.current += -0.3
 
       setTick((t) => t + 1)
       requestRef.current = requestAnimationFrame(step)
@@ -40,12 +46,18 @@ export function PlanetsBackground() {
     <div className='pointer-events-none fixed left-0 top-0 -z-10 h-full w-full'>
       <div className='relative h-full w-full'>
         <div className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'>
-          <AnimBase ox={70} oy={70}>
-            <AnimPlanet size={140} />
+          <AnimBase ox={70} oy={70} rotation={rotateSumRef.current}>
+            <AnimPlanet size={150} />
           </AnimBase>
 
-          <AnimBase x={posRef.current.x} y={posRef.current.y} ox={50} oy={50}>
-            <AnimPlanet size={100} baseColor='#f9d878' />
+          <AnimBase
+            x={posRef.current.x}
+            y={posRef.current.y}
+            ox={50}
+            oy={50}
+            rotation={rotatePlanetRef.current}
+          >
+            <AnimPlanet size={130} baseColor='#f9d878' />
           </AnimBase>
         </div>
       </div>
